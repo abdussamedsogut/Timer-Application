dd4cb170253e2d6f5f687ccfc96ac1249beb6d9a
Asamed <asamedsogut@hotmail.com>
2023-03-02 13:05:00 +0300

    Programming completed

--- Timer Application/ViewController.swift
+++ Timer Application/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer:Timer?
+    var counter1 = 1
+    var counter2 = 5
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        resultLabel.text = ""
     }
 
 
+    @IBAction func start(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countBackward), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func countForward() {
+        resultLabel.text = String(counter1)
+        counter1 += 1
+        
+        if counter1 > 6 {
+            timer?.invalidate()
+            resultLabel.text = "Time is up"
+            counter1 = 1
+        }
+    }
+    
+    
+    @objc func countBackward() {
+        resultLabel.text = String(counter2)
+        counter2 -= 1
+        
+        if counter2 < 0 {
+            timer?.invalidate()
+            resultLabel.text = "Time is up"
+            counter2 = 5
+        }
+    }
+    
 }
 
